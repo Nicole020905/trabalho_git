dfe3fd445af5a33b3d8ad0c303c65a977b0aafd7
"Nicole Alves" <nicolealvesdasilva05@gmail.com>
2023-02-23 11:35:50 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

--- TRABALHO_GIT_2I/src/Main.java
+++ TRABALHO_GIT_2I/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola mundo");
+		Scanner t = new Scanner (System.in);
+		System.out.println("Digite seu nome:");
+		String a = t.nextLine();
+		System.out.println("Olá "+ a );
+		System.out.println("Olá Mundo");
+		
+		
 		
 	}
 
